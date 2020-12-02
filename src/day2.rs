447c4b51646453b1aa40891cf483dd0979df7871
Théo Rozier <contact@theorozier.fr>
2020-12-02 20:27:36 +0100

    Comments removed.

--- src/day2.rs
+++ src/day2.rs
@@ -27,66 +27,6 @@ fn part2(inp: impl Iterator<Item=InputLine>) -> usize {
     }).count()
 }
 
-/*#[derive(Debug)]
-struct Constraint {
-    min: u16,
-    max: u16,
-    letter: char,
-    password: String
-}
-
-impl Constraint {
-
-    fn test_letters(&self) -> bool {
-        (self.min..=self.max).contains(&(self.password.chars().filter(|c| *c == self.letter).count() as u16))
-    }
-
-    fn test_one_in_range(&self) -> bool {
-        let a = self.password.chars().nth(self.min as usize - 1);
-        let b = self.password.chars().nth(self.max as usize - 1);
-        (a.is_some() && a.unwrap() == self.letter) ^ (b.is_some() && b.unwrap() == self.letter)
-    }
-
-}
-
-fn main() {
-
-    let t = INPUT.lines().map(String::from).collect::<Vec<String>>();
-
-    let constraints: Vec<Constraint> = INPUT.lines()
-        .map(|l| {
-
-            let parts: Vec<&str> = l.split(' ').collect();
-
-            let bounds: Vec<u16> = parts[0].split('-')
-                .map(u16::from_str)
-                .map(Result::unwrap)
-                .collect();
-
-            Constraint {
-                min: bounds[0],
-                max: bounds[1],
-                letter: parts[1].chars().nth(0).unwrap(),
-                password: parts[2].to_string()
-            }
-
-        })
-        .collect();
-
-    let x;
-
-    {
-        let test = 1;
-        x = &test;
-    }
-
-
-
-    println!("Count #1: {}", constraints.iter().filter(|c| c.test_letters()).count());
-    println!("Count #2: {}", constraints.iter().filter(|c| c.test_one_in_range()).count());
-
-}*/
-
 fn main() {
 
     println!("Part1: {}", part1(parse(INPUT.lines().map(String::from))));
