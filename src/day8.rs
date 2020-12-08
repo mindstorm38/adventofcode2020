0a1c20cafa99680180d128a3e6b3c20a5a27b58c
Théo Rozier <contact@theorozier.fr>
2020-12-08 19:45:42 +0100

    Added timings to test day8.

--- src/day8.rs
+++ src/day8.rs
@@ -1,36 +1,60 @@
 
 mod interpret;
+
+use std::time::Instant;
 use interpret::{Instruction, Interpreter, ExitCause};
 
 const INPUT: &str = include_str!("res/day8");
 
 fn main() {
 
-    let program = interpret::parse(INPUT.lines().map(String::from));
-    let mut interpreter = Interpreter::new();
+    let whole_now = Instant::now();
 
+    let now = Instant::now();
+    let program = interpret::parse(INPUT.lines().map(String::from));
+    println!("Parsing duration: {}ns", now.elapsed().as_secs_f64());
+
+    let now = Instant::now();
+    let mut interpreter = Interpreter::new();
+    println!("New duration: {}ns", now.elapsed().as_nanos());
+
+    let now = Instant::now();
     interpreter.load_program(&program);
     interpreter.run();
+    println!("Run: {}ns", now.elapsed().as_nanos());
     println!("Acc: {}", interpreter.get_accumulator());
 
+    let now = Instant::now();
     let mut fix_index = 0usize;
 
     loop {
 
         interpreter.load_program(&program);
 
-        if let Some(inst) = interpreter.get_instruction_mut(fix_index) {
-            use Instruction::*;
-            match *inst {
-                Jmp(val) => *inst = Nop(val),
-                Nop(val) => *inst = Jmp(val),
-                _ => {}
-            }
-        } else {
-            panic!("Can no longer switch instruction.");
-        }
+        loop {
 
-        fix_index += 1;
+            if let Some(inst) = interpreter.get_instruction_mut(fix_index) {
+
+                fix_index += 1;
+
+                use Instruction::*;
+                match *inst {
+                    Jmp(val) => {
+                        *inst = Nop(val);
+                        break;
+                    },
+                    Nop(val) => {
+                        *inst = Jmp(val);
+                        break;
+                    },
+                    _ => {}
+                }
+
+            } else {
+                panic!("Can no longer swap instruction.");
+            }
+
+        }
 
         if let Some(ExitCause::EndOfProgram) = interpreter.run() {
             break;
@@ -38,6 +62,8 @@ fn main() {
 
     }
 
+    println!("Bug resolution: {}ns", now.elapsed().as_nanos());
     println!("End of Program, switched the instruction at {}, accumulator: {}", fix_index, interpreter.get_accumulator());
+    println!("Whole run: {}ns", whole_now.elapsed().as_nanos());
 
 }
