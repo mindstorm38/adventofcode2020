88fe419c3ee593cf17357d4a0e5cbc7c78b1f41f
Théo Rozier <contact@theorozier.fr>
2020-12-15 14:32:44 +0100

    Day 15 fix (40s in debug, 2.8s in release).

--- src/day15.rs
+++ src/day15.rs
@@ -11,50 +11,33 @@ struct NumberStat {
 
 fn run_sequence(numbers: &Vec<u32>, times: usize) -> (f64, f64, u32) {
 
-    let mut sequence = numbers.clone();
-    let mut cache: HashMap<u32, NumberStat> = HashMap::new();
+    let now = Instant::now();
+    let loops = times - numbers.len();
 
-    for (i, &n) in sequence.iter().enumerate() {
-        cache.insert(n, NumberStat {
-            index: i,
-            first: true
-        });
+    let mut last_value = *numbers.last().unwrap();
+    let mut cache: HashMap<u32, usize> = HashMap::new();
+
+    for (i, &n) in numbers.iter().enumerate() {
+        cache.insert(n, i);
     }
 
-    let now = Instant::now();
-    let loops = times - sequence.len();
+    for i in numbers.len()..times {
 
-    for i in sequence.len()..times {
-
-        let num = sequence[i - 1];
-        let stat = cache.get_mut(&num).unwrap();
-
-        let new = if stat.first {
-            0
+        let value = if let Some(&j) = cache.get(&last_value) {
+            (i - 1 - j) as u32
         } else {
-            let res = (i - 1 - stat.index) as u32;
-            stat.index = i - 1;
-            res
+            0
         };
 
-        sequence.push(new);
-
-        if cache.contains_key(&new) {
-            let new_stat = cache.get_mut(&new).unwrap();
-            new_stat.first = false;
-        } else {
-            cache.insert(new, NumberStat {
-                index: i,
-                first: true
-            });
-        }
+        cache.insert(last_value, i - 1);
+        last_value = value;
 
     }
 
     let total_time = now.elapsed().as_secs_f64();
     let average_loop = total_time / loops as f64;
 
-    (total_time, average_loop, *sequence.last().unwrap())
+    (total_time, average_loop, last_value)
 
 }
 
